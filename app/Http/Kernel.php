1ee42f43ad368ec1649496f2c6c250fdfe22d490
yuhsing <bob301833@gmail.com>
2017-07-19 14:33:34 +0800

    middleware - custom method and 404 page

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,6 +49,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'admin' => \Illuminate\Routing\Middleware\Admin::class,
+        'admin' => \App\http\Middleware\Admin::class,
     ];
 }
