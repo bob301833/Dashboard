8e93cec9fce95c5cfa5797c573e268216ab7c243
yuhsing <bob301833@gmail.com>
2017-07-19 12:02:30 +0800

    Update User

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\AdminUsersEditRequest;
 use App\Http\Requests;
 use App\User;
 use App\Role;
@@ -43,7 +44,18 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        $input = $request->all();
+
+        if(trim($request->password) == ''){
+            
+            $input = $request->except('password');
+            
+        }else{
+
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+            
+        }
+
 
         if($file = $request->file('file')){
 
@@ -57,8 +69,6 @@ class AdminUsersController extends Controller
 
         }
 
-        $input['password'] = bcrypt($request->password);
-
         User::create($input);
         return redirect('admin/users');
     }
@@ -71,7 +81,7 @@ class AdminUsersController extends Controller
      */
     public function show($id)
     {
-        return view('admin.users.show');
+        //return view('admin.users.show');
     }
 
     /**
@@ -94,9 +104,36 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(AdminUsersEditRequest $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        if(trim($request->password == '')){
+            $input = $request->except('password');            
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+
+
+        if($file = $request->file('file')){
+            $name = time().$file->getClientOriginalName();
+
+            $file->move('images',$name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        // if($request->password){
+        //     $input['password']
+        // }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
     }
 
     /**
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -4,53 +4,59 @@
 
     <h1>Edit Users</h1>
 
-    <div class="col-sm-3">
-    
-        <img src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt="" class="img-responsive img-rounded">
+    <div class="row">
+
+        <div class="col-sm-3">
+        
+            <img src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt="" class="img-responsive img-rounded">
+
+        </div>
+
+        <div class="col-sm-9">
+            {!! Form::model([$user,$roles],['method'=>'PATCH', 'action'=>['AdminUsersController@update',$user->id], 'files'=>true]) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('name', 'Name:') !!}
+                    {!! Form::text('name', $user->name, ['class'=>'form-control']) !!}
+                </div>
+            
+                <div class="form-group">
+                    {!! Form::label('email', 'Email:') !!}
+                    {!! Form::email('email', $user->email, ['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('role_id', 'Role:') !!}
+                    {!! Form::select('role_id',[''=>'Choose Options'] + $roles ,$user->role_id, ['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('is_active', 'Status:') !!}
+                    {!! Form::select('is_active', [0 => 'Not Active', 1 => 'Actuve'], $user->is_active, ['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('file', 'File:') !!}
+                    {!! Form::file('file', null, ['class'=>'form-control']) !!}
+                </div>
+            
+                <div class="form-group">
+                    {!! Form::label('password', 'Password:') !!}
+                    {!! Form::password('password', ['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::submit('Update User',['class' => 'btn btn-primary']) !!}
+                </div>
+            
+            {!! Form::close() !!}
+        </div>
 
     </div>
 
-    <div class="col-sm-9">
-        {!! Form::model([$user,$roles],['method'=>'PATCH', 'action'=>['AdminUsersController@update',$user->id], 'files'=>true]) !!}
-        
-            <div class="form-group">
-                {!! Form::label('name', 'Name:') !!}
-                {!! Form::text('name', $user->name, ['class'=>'form-control']) !!}
-            </div>
-        
-            <div class="form-group">
-                {!! Form::label('email', 'Email:') !!}
-                {!! Form::email('email', $user->email, ['class'=>'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('role_id', 'Role:') !!}
-                {!! Form::select('role_id',[''=>'Choose Options'] + $roles ,'', ['class'=>'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('is_active', 'Status:') !!}
-                {!! Form::select('is_active', [0 => 'Not Active', 1 => 'Actuve'], $user->is_active, ['class'=>'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('file', 'File:') !!}
-                {!! Form::file('file', null, ['class'=>'form-control']) !!}
-            </div>
-        
-            <div class="form-group">
-                {!! Form::label('password', 'Password:') !!}
-                {!! Form::password('password', ['class'=>'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::submit('Create User',['class' => 'btn btn-primary']) !!}
-            </div>
-        
-        {!! Form::close() !!}
+    <div class="row">
+        @include('includes.form_error')
     </div>
-    @include('includes.form_error')
-
 
 
 @endsection
