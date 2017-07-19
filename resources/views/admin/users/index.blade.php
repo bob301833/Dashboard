c3c36dbf5699d3e7f93533e819b13fe4e172da04
yuhsing <bob301833@gmail.com>
2017-07-19 10:56:36 +0800

    Edit user

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -22,8 +22,8 @@
                 @foreach($users as $user)
                     <tr>
                         <td>{{$user->id}}</td>
-                        <td><img height="50" src="{{$user->photo ? $user->photo->file : 'No user photo'}}" alt=""></td>
-                        <td>{{$user->name}}</td>
+                        <td><img height="50" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
+                        <td><a href="{{route('admin.users.edit',$user->id)}}">{{$user->name}}</a></td>
                         <td>{{$user->email}}</td>
                         <td>{{$user->role ? $user->role->name :'User has no role'}}</td>
                         <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
