a5ef4c905a1d7cf4aee5067e22c4b1d6ae428e4d
yuhsing <bob301833@gmail.com>
2017-07-26 11:21:51 +0800

    Creating model and migration for categories

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -15,7 +15,7 @@
         
             <div class="form-group">
                 {!! Form::label('category_id', 'Category:') !!}
-                {!! Form::select('category_id', [''=>'options'],'', ['class'=>'form-control']) !!}
+                {!! Form::select('category_id', [0=>'Javascript', 1=>'PHP'],'', ['class'=>'form-control']) !!}
             </div>
 
             <div class="form-group">
