b2799099f6b859403c56f075a44b32f3531eebb0
yuhsing <bob301833@gmail.com>
2017-07-26 11:29:05 +0800

    Displaying and creating posts with categories

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -15,7 +15,7 @@
         
             <div class="form-group">
                 {!! Form::label('category_id', 'Category:') !!}
-                {!! Form::select('category_id', [0=>'Javascript', 1=>'PHP'],'', ['class'=>'form-control']) !!}
+                {!! Form::select('category_id', [''=>'Choose Categories']+$categories,'', ['class'=>'form-control']) !!}
             </div>
 
             <div class="form-group">
