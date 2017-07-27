c07cdb5230f9144be36dbbb6e605da153ae2822f
yuhsing <bob301833@gmail.com>
2017-07-27 09:58:54 +0800

    Setting up and displaying media

---
+++ resources/views/admin/media/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    <h1>Media</h1>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Created</th>
+        </tr>
+        </thead>
+        <tbody>
+            @if($photos)
+                @foreach($photos as $photo)
+                    <tr>
+                        <td>{{$photo->id}}</td>
+                        <td><img height=50 src="{{$photo->file}}" alt=""></td>
+                        <td>{{$photo->created_at ? $photo->created_at : 'no date'}}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
+    
+@endsection
