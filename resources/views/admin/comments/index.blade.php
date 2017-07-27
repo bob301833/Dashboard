98616a4e7c384a915ad839c6e469e78a1dc64542
yuhsing <bob301833@gmail.com>
2017-07-27 11:57:47 +0800

    Setup Comment view

---
+++ resources/views/admin/comments/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin')
+
+@section('content')
+    
+    <h1>Comments</h1>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        
+        <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+        </tr>
+        </tbody>
+    </table>
+
+@endsection
