4c2b80a1f46555e2a4d650832d9214731fc4d0a2
yuhsing <bob301833@gmail.com>
2017-07-27 14:32:58 +0800

    Create a post

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -139,4 +139,10 @@ class AdminPostsController extends Controller
         return redirect('/admin/posts');
 
     }
+
+    public function post($id){
+        $post = Post::findOrFail($id);
+
+        return view('post',compact('post'));
+    }
 }
