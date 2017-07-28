9652c17986684766b362c87e16558e42957609c5
yuhsing <bob301833@gmail.com>
2017-07-28 10:21:39 +0800

    Displaying replies

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -89,17 +89,29 @@
                     <p>{{$comment->body}}</p>
 
                     <!-- Nested Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
 
+                        
+
+                    <div id="nseted-comment" class="media">
+
+                        @if(count($comment->replies)>0)
+                        
+                            @foreach($comment->replies as $reply)
+                                <div class="media">
+                                    <a class="pull-left" href="#">
+                                        <img height="64" class="media-object" src="{{$reply->photo}}" alt="">
+                                    </a>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">{{$reply->author}}
+                                            <small>{{$reply->created_at->diffForHumans()}}</small>
+                                        </h4>
+                                        <p>{{$reply->body}}</p>
+                                    </div>
+                                </div>
+                            @endforeach
+                            
+                        @endif
+                        
                         {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply']) !!}
                         
                             <div class="form-group">
@@ -119,6 +131,8 @@
 
                     </div>
                     <!-- End Nested Comment -->
+
+
                 </div>
             </div>
 
